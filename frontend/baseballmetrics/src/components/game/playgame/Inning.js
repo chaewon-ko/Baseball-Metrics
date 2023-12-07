c3df2ac7f1a5544e8f1114829b602b8bc95a94f9
changu81 <ydi990801@gmail.com>
2023-12-07 10:43:32 +0900

    fix inning

--- frontend/baseballmetrics/src/components/game/playgame/Inning.js
+++ frontend/baseballmetrics/src/components/game/playgame/Inning.js
@@ -2,92 +2,63 @@ import React from 'react';
 import styled from 'styled-components';
 
 const GridContainer = styled.div`
-	border: 1px solid white;
-	border-radius: 1rem;
-	padding: 10px;
-	display: grid;
-	grid-template-columns: repeat(12,1fr);
-
-	width: 800px;
-	margin: 0 auto;
-`
+  border: 1px solid white;
+  border-radius: 1rem;
+  padding: 10px;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  width: 800px;
+  margin: 0 auto;
+`;
 
 const GridItem = styled.div`
-	display: grid;
-	grid-template-rows: 1fr 1fr;
-`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+`;
+
 const Top = styled.div`
-	padding-bottom: 10px;
-	border-bottom: 1px solid white;
-`
+  padding-bottom: 10px;
+  border-bottom: 1px solid white;
+`;
 
 const Bot = styled.div`
-	padding-top: 10px;
-`
+  padding-top: 10px;
+`;
 
 const Top9 = styled.div`
-	padding-bottom: 10px;
-	border-bottom: 1px solid white;
-	border-right: 1px solid white;
-`
+  padding-bottom: 10px;
+  border-bottom: 1px solid white;
+  border-left: 1px solid white;
+`;
+
 const Bot9 = styled.div`
-	padding-top: 10px;
-	border-right: 1px solid white;
-`
-	
+  padding-top: 10px;
+  border-left: 1px solid white;
+`;
+
 const Inning = () => {
-	return (
-		<GridContainer>
-			<GridItem>
-				<Top>1</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>2</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>3</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>4</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>5</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>6</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>7</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>8</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top9>9</Top9>
-				<Bot9>0</Bot9>
-			</GridItem>
-			<GridItem>
-				<Top>R</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>H</Top>
-				<Bot>0</Bot>
-			</GridItem>
-			<GridItem>
-				<Top>B</Top>
-				<Bot>0</Bot>
-			</GridItem>
-		</GridContainer>
-	);
+  const inning = [1, 2, 3, 4, 5, 6, 7, 8, 9, "R", "H", "B"];
+	// 나중에 데이터 받아와서 이용하기!
+  const botData = [1, 0, 0, 2, 3, 2, 0, 0, 2, 2, 12, 5];
+
+  return (
+    <GridContainer>
+      {inning.map((item, index) => (
+        <GridItem key={index}>
+          {index === 9 ? (
+            <Top9>{item}</Top9>
+          ) : (
+            <Top>{item}</Top>
+          )}
+          {index === 9 ? (
+            <Bot9>{botData[index]}</Bot9>
+          ) : (
+            <Bot>{botData[index]}</Bot>
+          )}
+        </GridItem>
+      ))}
+    </GridContainer>
+  );
 };
 
 export default Inning;
