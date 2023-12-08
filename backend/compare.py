f73c6fbc38890b113d1c252f274869d313f6b6ba
신현창 <sinhyeonchang@shc.local>
2023-12-08 23:36:45 +0900

    testing

--- backend/compare.py
+++ backend/compare.py
@@ -1,22 +1,21 @@
 import pandas as pd
 
-abil = pd.read_csv("../data/Abill/battersAbill.csv")
-abil = abil
-basic = pd.reas_csv("../data/battersData/battersBasic.csv")
-geo1 = abil.loc[abil["이름"] == "양의지"].values.tolist()
-geo2 = abil.loc[abil["이름"] == "홍창기"].values.tolist()
 
-bar1 = basic.loc[abil["이름"] == "양의지"].values.tolist()
-bar2 = basic.loc[abil["이름"] == "홍창기"].values.tolist()
 
 
 
 def abilCompare(type, name1, name2):
-    abil = pd.read_csv("../data/Abill/battersAbill.csv")
-    abil = abil
-    basic = pd.reas_csv("../data/battersData/battersBasic.csv")
+    if(type == "pithcer"):
+        abil = pd.read_csv("../data/Abill/battersAbill.csv")
+        basic = pd.read_csv("../data/battersData/battersBasic.csv")
+    else :
+        abil = pd.read_csv("../data/Abill/pitchersAbill.csv")
+        basic = pd.read_csv("../data/pitcherssData/pitchersBasic.csv")
     geo1 = abil.loc[abil["이름"] == name1].values.tolist()
     geo2 = abil.loc[abil["이름"] == name2].values.tolist()
     bar1 = basic.loc[abil["이름"] == name1].values.tolist()
     bar2 = basic.loc[abil["이름"] == name2].values.tolist()
     return geo1, geo2, bar1, bar2
+
+
+print(abilCompare("batter", "양의지","홍창기"))
