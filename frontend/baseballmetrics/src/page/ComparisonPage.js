47abaea2a4ac851bea2ee32d06ebe0aac75d79ba
changu81 <ydi990801@gmail.com>
2023-12-09 19:25:24 +0900

    connect compare to backend

--- frontend/baseballmetrics/src/page/ComparisonPage.js
+++ frontend/baseballmetrics/src/page/ComparisonPage.js
@@ -3,6 +3,8 @@ import RadarGraph from '../components/comparison/RadarGraph';
 import styled from 'styled-components';
 import BarGraph from '../components/comparison/BarGraph';
 import { playerList } from '../players';
+import axios from 'axios';
+import { Radar } from 'react-chartjs-2';
 
 const GraphDiv = styled.div`
   margin: 0 auto;
@@ -57,38 +59,64 @@ const teamList = [
 ]
 // 통신으로 받아와야할 선수들 데이터 선수 두명 + 해당 선수 구단/리그 평균(11개 데이터 이건 그냥 프론트에 박아놓을까?)
 const ComparisonPage = ({ theme }) => {
-  const [selectedPlayer1, setSelectedPlayer1] = useState(playerList[0]);
-  const [selectedPlayer2, setSelectedPlayer2] = useState(playerList[1]);
+  const [selectedPlayer1, setSelectedPlayer1] = useState(null);
+  const [selectedPlayer2, setSelectedPlayer2] = useState(null);
   const [selectedTeam1, setSelectedTeam1] = useState(teamList[0]);
   const [selectedTeam2, setSelectedTeam2] = useState(teamList[0]);
   const [filteredPlayers1, setFilteredPlayers1] = useState([]);
   const [filteredPlayers2, setFilteredPlayers2] = useState([]);
+  const [player1, setPlayer1] = useState({ name: '', geo: [], bar: [] });
+  const [player2, setPlayer2] = useState({ name: '', geo: [], bar: [] });
 
   useEffect(() => {
     // Player 1을 위해 선택된 팀에 따라 선수 필터링
-    const playersForTeam1 = playerList.filter(player => player.teamId === selectedTeam1.id);
+    const playersForTeam1 = filteredPlayers(selectedTeam1.id);
     setFilteredPlayers1(playersForTeam1);
 
     // Player 2을 위해 선택된 팀에 따라 선수 필터링
-    const playersForTeam2 = playerList.filter(player => player.teamId === selectedTeam2.id);
+    const playersForTeam2 = filteredPlayers(selectedTeam2.id);
     setFilteredPlayers2(playersForTeam2);
   }, [selectedTeam1, selectedTeam2]);
 
+  const filteredPlayers = (teamId) => playerList.filter(player => player.teamId === teamId);
+
+  const handlePlayerSelect = async () => {
+    try {
+      const response = await axios.post('/compare', {
+        "type": 'batter',
+        "player1": selectedPlayer1.name,
+        "player2": selectedPlayer2.name,
+      });
+
+      setPlayer1({
+        name: selectedPlayer1.name,
+        geo: response.data.geo1,
+        bar: response.data.bar1,
+      });
+
+      setPlayer2({
+        name: selectedPlayer2.name,
+        geo: response.data.geo2,
+        bar: response.data.bar2,
+      });
+    } catch (error) {
+      console.error('Error fetching player data:', error.message);
+    }
+  };
+  
   const handlePlayer1Select = (player) => {
     setSelectedPlayer1(player);
   };
-
   const handlePlayer2Select = (player) => {
     setSelectedPlayer2(player);
   };
-
   const handleTeam1Select = (team) => {
     setSelectedTeam1(team);
   };
-
   const handleTeam2Select = (team) => {
     setSelectedTeam2(team);
   };
+  
   return (
     <div>
       <div>
@@ -96,6 +124,7 @@ const ComparisonPage = ({ theme }) => {
         <SelectPlayer>
           <label>선수 1: </label>
           <Styledselect1 onChange={(e) => handleTeam1Select(teamList.find((p) => p.id === Number(e.target.value)))}>
+            <option value="" disabled selected>팀 선택</option>
             {teamList.map((team) => (
               <option key={team.id} value={team.id}>
                 {team.name}
@@ -103,6 +132,7 @@ const ComparisonPage = ({ theme }) => {
             ))}
           </Styledselect1>
           <Styledselect1 onChange={(e) => handlePlayer1Select(filteredPlayers1.find((p) => p.id === Number(e.target.value)))}>
+           <option value="" disabled selected>선수 선택</option>
             {filteredPlayers1.map((player) => (
               <option key={player.id} value={player.id}>
                 {player.name}
@@ -113,6 +143,7 @@ const ComparisonPage = ({ theme }) => {
         <SelectPlayer>
           <label>선수 2: </label>
           <Styledselect2 onChange={(e) => handleTeam2Select(teamList.find((p) => p.id === Number(e.target.value)))}>
+          <option value="" disabled selected>팀 선택</option>
             {teamList.map((team) => (
               <option key={team.id} value={team.id}>
                 {team.name}
@@ -120,6 +151,7 @@ const ComparisonPage = ({ theme }) => {
             ))}
           </Styledselect2>
           <Styledselect2 onChange={(e) => handlePlayer2Select(filteredPlayers2.find((p) => p.id === Number(e.target.value)))}>
+            <option value="" disabled selected>선수 선택</option>
             {filteredPlayers2.map((player) => (
               <option key={player.id} value={player.id}>
                 {player.name}
@@ -128,15 +160,16 @@ const ComparisonPage = ({ theme }) => {
           </Styledselect2>
         </SelectPlayer>
       </div>
+      <button onClick={handlePlayerSelect} >COMPARE</button>
       <h2>RadarGraph</h2>
       <GraphDiv>
-        <RadarGraph theme={theme} player1={selectedPlayer1.abilities} player2={selectedPlayer2.abilities} />
+        <RadarGraph theme={theme} player1={player1.geo} player2={player2.geo} />
       </GraphDiv>
       <h2>Bar Graph</h2>
       <GraphDiv3>
-        <BarGraph theme={theme} player1={selectedPlayer1.abilities} player2={selectedPlayer2.abilities} />
-        <BarGraph theme={theme} player1={selectedPlayer1.abilities} player2={selectedPlayer2.abilities} />
-        <BarGraph theme={theme} player1={selectedPlayer1.abilities} player2={selectedPlayer2.abilities} />
+        <BarGraph theme={theme} player1={player1.bar} player2={player2.bar} />
+        {/* <BarGraph theme={theme} player1={selectedPlayer1.bar} player2={selectedPlayer2.bar} />
+        <BarGraph theme={theme} player1={selectedPlayer1.bar} player2={selectedPlayer2.bar} /> */}
       </GraphDiv3>
     </div>
   );
