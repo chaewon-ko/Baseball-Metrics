045eed6cdc5ff7cfc74d3c4f277bd212bd6221b7
신현창 <sinhyeonchang@shc.local>
2023-12-10 00:34:11 +0900

    hotfix

--- backend/page.py
+++ backend/page.py
@@ -6,7 +6,7 @@ all = pd.read_csv('./data/battersAll.csv')
 def PageData(i,sort):
     all2 = all.sort_values(by=[sort], ascending=False)[(i-1)*30:i*30]
     all2 = all2[["이름","팀/포지션","타율","볼넷","삼진","병살","장타","출루","득점권타율","phLI","FO/GO", "BB/K"]]
-    all2.to_csv('../data/response/response.csv')
+    all2.to_csv('../data/response/response.csv',index=False)
 
 
 def FilterTeam(team):
@@ -15,7 +15,9 @@ def FilterTeam(team):
     all2 = all2[filtering]
     all2 = all2.sort_values(by=['타율'], ascending=False)
     all2 = all2[["이름","팀/포지션","타율","볼넷","삼진","병살","장타","출루","득점권타율","phLI","FO/GO", "BB/K"]]
-    all2.to_csv('../data/response/response.csv')
+    all2.to_csv('../data/response/response.csv', index=False)
 
+
+PageData(1,'타율')
 
 
