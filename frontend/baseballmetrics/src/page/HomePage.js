2ca1b7e428a96a7af64e59f1b8ea727f62c210f3
chaewon-ko <Github njs0657@naver.com>
2023-12-15 03:46:08 +0900

    home page

--- frontend/baseballmetrics/src/page/HomePage.js
+++ frontend/baseballmetrics/src/page/HomePage.js
@@ -5,21 +5,24 @@ import styled from 'styled-components';
 const Background =styled.div`
 	background-color: lightgray;
 	height: 100vh;
+	background-image: url('/image/home.png');
+	background-size: cover;
+	background-position: center;
 `
 
 const StyledLink = styled(Link)`
 	text-decoration: none;
-	background-color: black;
-	border-radius: 1rem;
-	padding: 15px;
+	padding: 18px 77px;
 	margin-right: 20px;
 	color: white;
-	font-size: 20pt;
+	font-size: 14pt;
+	border-radius: 21px;
+	border: 1px solid #FFF;
 `
 const Links = styled.div`
 	position: fixed;
-	bottom: 200px;
-	right: 200px;
+	top: 630px;
+	left: 72px;
 `
 
 
@@ -27,9 +30,8 @@ const HomePage = () => {
 	return (
 		<Background>
 			<Links>
-				<p>이곳은 홈페이지입니다. 아래 버튼을 클릭하여 게임/기록 페이지로 이동</p><br/>
-				<StyledLink to={"/game"}>Game</StyledLink>
-				<StyledLink to={"/record"}>Record</StyledLink>
+				<StyledLink to={"/game"}>게임하기</StyledLink>
+				<StyledLink to={"/compare"}>기록 분석하기</StyledLink>
 			</Links>
 		</Background>
 	);
